1aae281a506d499033d2d4bb984f8a8203edcf86
Howard Zeng <50036765+howie-zeng@users.noreply.github.com>
2024-12-08 21:10:33 -0500

    update

--- R/data_processing.R
+++ R/data_processing.R
@@ -13,57 +13,68 @@ process_all_dataframes <- function(df_list) {
   for (name in names(df_list)) {
     df <- df_list[[name]]
 
-    if (name == "HR Dataset") {
-      # HR Dataset Processing
-      df <- df %>%
-        rename(relevant_experience = relevent_experience) %>%
-        mutate(
-          relevant_experience = str_replace(
-            relevant_experience,
-            pattern = "relevent",
-            replacement = "relevant"
+    tryCatch({
+      if (name == "HR Dataset") {
+        df <- df %>%
+          rename(relevant_experience = relevent_experience) %>%
+          mutate(
+            relevant_experience = str_replace(
+              relevant_experience,
+              pattern = "relevent",
+              replacement = "relevant"
+            )
+          ) %>%
+          mutate_if(is.character, as.factor) %>%
+          mutate(target = as.factor(target)) %>%
+          mutate(city_development_index = as.numeric(city_development_index)) %>%
+          select(-enrollee_id, -city, -gender)
+      } else if (name == "Bank Fraud Dataset") {
+        df <- df %>%
+          rename(target = fraud_bool)
+      } else if (name == "Credit Card Fraud Dataset") {
+        df <- df %>%
+          rename(target = Class)
+      } else if (name == "Diabetes Dataset") {
+        df <- df %>%
+          rename(target = Diabetes_binary) %>%
+          select(-Fruits, -Veggies, -Sex, -CholCheck, -AnyHealthcare)
+      } else if (name == "Insurance Dataset") {
+        df <- df %>%
+          rename(target = Response)
+      } else if (name == "Credit Card Default Dataset") {
+        if (!all(c("EDUCATION", "MARRIAGE") %in% colnames(df))) {
+          stop("Missing required columns in Credit Card Default Dataset")
+        }
+        df <- df %>%
+          rename(target = default.payment.next.month) %>%
+          mutate(
+            EDUCATION = ifelse(EDUCATION %in% c(0, 5, 6), 4, EDUCATION),
+            MARRIAGE = ifelse(MARRIAGE == 0, 3, MARRIAGE)
+          ) %>%
+          mutate(
+            Payment_Sum = PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6,
+            Dues = BILL_AMT1 + BILL_AMT2 + BILL_AMT3 - (PAY_AMT1 + PAY_AMT2 + PAY_AMT3)
+          ) %>%
+          mutate(
+            SEX = factor(SEX, levels = c(1, 2), labels = c("MALE", "FEMALE")),
+            EDUCATION = factor(EDUCATION, labels = c("Graduate School", "University", "High School", "Others")),
+            MARRIAGE = factor(MARRIAGE, labels = c("Married", "Single", "Others"))
           )
-        ) %>%
-        mutate_if(is.character, as.factor) %>%
-        mutate(target = as.factor(target)) %>%
-        mutate(city_development_index = as.numeric(city_development_index)) %>%
-        select(-enrollee_id, -city, -gender)
-    } else if (name == "Bank Fraud Dataset") {
-      # Bank Fraud Dataset Processing
+      } else if (name == "Credit Card Approval Dataset") {
+        df_credit_data <- read.csv("data/credit_record.csv", stringsAsFactors = FALSE)
+        df <- process_credit_card_data(df_credit_data, df)
+      }
 
-    } else if (name == "Credit Card Fraud Dataset") {
-      # Credit Card Fraud Dataset Processing
+      # Apply Label Encoding to All Categorical Columns
+      categorical_columns <- sapply(df, is.factor) | sapply(df, is.character)
+      df[categorical_columns] <- lapply(df[categorical_columns], as.factor)
 
-    } else if (name == "Diabetes Dataset") {
-      # Diabetes Dataset Processing
-      df <- df %>%
-        select(-Fruits, -Veggies, -Sex, -CholCheck, -AnyHealthcare)
-    } else if (name == "Insurance Dataset") {
-      # Insurance Dataset Processing
-
-    } else if (name == "Credit Card Default Dataset") {
-      # Credit Card Default Dataset Processing
-      df <- df %>%
-        mutate(
-          EDUCATION = ifelse(EDUCATION %in% c(0, 5, 6), 4, EDUCATION),
-          MARRIAGE = ifelse(MARRIAGE == 0, 3, MARRIAGE)
-        ) %>%
-        mutate(
-          Payment_Sum = PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6,
-          Dues = BILL_AMT1 + BILL_AMT2 + BILL_AMT3 - (PAY_AMT1 + PAY_AMT2 + PAY_AMT3)
-        ) %>%
-        mutate(
-          SEX = factor(SEX, levels = c(1, 2), labels = c("MALE", "FEMALE")),
-          EDUCATION = factor(EDUCATION, labels = c("Graduate School", "University", "High School", "Others")),
-          MARRIAGE = factor(MARRIAGE, labels = c("Married", "Single", "Others"))
-        )
-    } else if (name == "Credit Card Approval Dataset") {
-      # Credit Card Approval Dataset Processing
-
-    }
-
-    # Add the processed dataframe to the list
-    processed_list[[name]] <- df
+      # Store processed dataframe
+      processed_list[[name]] <- df
+    }, error = function(e) {
+      warning(paste("Error processing dataset:", name, "\n", e$message))
+      processed_list[[name]] <- NULL  # Add NULL if the dataset cannot be processed
+    })
   }
 
   return(processed_list)
