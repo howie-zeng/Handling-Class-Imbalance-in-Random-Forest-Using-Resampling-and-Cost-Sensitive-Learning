2b8dfb2058efcb93faad23702fe97a5df678acd9
Howard Zeng <50036765+howie-zeng@users.noreply.github.com>
2024-12-16 17:28:36 -0500

    update

--- R/class_balance_bagging_boosting.R
+++ R/class_balance_bagging_boosting.R
@@ -563,38 +563,56 @@ SMOTETomek <-
     #   k       : Number of nearest neighbors in SMOTE
 
     newData <- SMOTE(x, y, percOver, k)
-    tgt <- length(newData)
-    indexTL <- TomekLink(tgt, newData)
+    indexTL <- TomekLink('target', newData)
     newDataRemoved <- newData[!indexTL, ]
     return(newDataRemoved)
   }
 #' @export
 TomekLink <-
-  function(tgt, data)
-  {
-    # Identify Tomek links
-    indexTomek <- rep(FALSE, nrow(data))
-    classTable <- table(data[, tgt])
-    majCl <- names(which.max(classTable))
-    minCl <- names(which.min(classTable))
-    indexMin <- which(data[, tgt] == minCl)
+    function(tgt='target', data)
+        # Inputs:
+        #   form: model formula
+        #   data: dataset
+        # Output:
+        #   logical vector indicating whether a instance is in TomekLinks
+    {
 
-    dataTransformed <- Numeralize(data[, -tgt])
+        indexTomek <- rep(FALSE, nrow(data))
 
-    require("RANN")
-    indexOrder1 <- nn2(dataTransformed, dataTransformed[indexMin, ], k = 2)$nn.idx
-    indexTomekCa <- data[indexOrder1[, 2], tgt] == majCl
-    if (sum(indexTomekCa) > 0) {
-      TomekCa <- cbind(indexMin[indexTomekCa], indexOrder1[indexTomekCa, 2])
-      indexOrder2 <- nn2(dataTransformed, dataTransformed[TomekCa[, 2], ], k = 2)$nn.idx
-      indexPaired <- indexOrder2[, 2] == TomekCa[, 1]
-      if (sum(indexPaired) > 0) {
-        indexTomek[TomekCa[indexPaired, 1]] <- TRUE
-        indexTomek[TomekCa[indexPaired, 2]] <- TRUE
-      }
+        # find the column of class variable
+        classTable <- table(data[, tgt])
+
+        # seperate the group
+        majCl <- names(which.max(classTable))
+        minCl <- names(which.min(classTable))
+
+        # get the instances of the larger group
+        indexMin <- which(data[, tgt] == minCl)
+        #numMin  <- length(indexMin)
+
+
+        # convert dataset in numeric matrix
+        dataTransformed <- Numeralize(data[, -tgt])
+
+        # generate indicator matrix
+        require("RANN")
+        indexOrder1  <- nn2(dataTransformed, dataTransformed[indexMin, ], k = 2)$nn.idx
+        indexTomekCa <- data[indexOrder1[, 2], tgt] == majCl
+        if (sum(indexTomekCa) > 0)
+        {
+            TomekCa <- cbind(indexMin[indexTomekCa],indexOrder1[indexTomekCa, 2])
+
+            # find nearest neighbour of potential majority instance
+            indexOrder2 <- nn2(dataTransformed, dataTransformed[TomekCa[, 2], ], k = 2)$nn.idx
+            indexPaired <- indexOrder2[ ,2] == TomekCa[, 1]
+            if (sum(indexPaired) > 0)
+            {
+                indexTomek[TomekCa[indexPaired, 1]] <- TRUE
+                indexTomek[TomekCa[indexPaired, 2]] <- TRUE
+            }
+        }
+        return(indexTomek)
     }
-    return(indexTomek)
-  }
 #' @export
 SMOTE <-
   function(x, y, percOver = 1400, k = 5)
